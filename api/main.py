6c4ca541e4de19c18b5d0eb238c81ab9f87bf245
krfx502 <mihaikenj@gmail.com>
2026-07-25 05:09:49 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import oracledb, os
+oracledb.init_oracle_client(lib_dir="/home/azureuser/oracle/instantclient_19_21")
 app = FastAPI()
 
 class UserRegistration(BaseModel):
