4fa3ebf5e4b555904d17852e786c50ca2e0caa87
krfx502 <mihaikenj@gmail.com>
2026-07-25 04:33:26 +0300

    database oracle 11g

---
+++ api/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import oracledb, os
+app = FastAPI()
+
+class UserRegistration(BaseModel):
+    username: str
+    password: str
+
+@app.post("/register")
+def register(user: UserRegistration):
+    try:
+        dsn_oracle = oracledb.makedsn("keyrif.me", 1521, sid="xe")
+
+        connection = oracledb.connect(
+            user="citadel",
+            password=os.getenv("DB_PASSWORD"),
+            dsn=dsn_oracle
+        )
+
+        cursor = connection.cursor()
+
+        cursor.callproc("create_account", [user.username, user.password])
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+        return {"status": "succes"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
